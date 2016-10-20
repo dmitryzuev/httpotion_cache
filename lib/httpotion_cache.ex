cde1fbe54cbb1d43e8f134602141bd0afc8111ce
Dmitry Zuev <d.zuev@rambler-co.ru>
2016-10-20 13:07:42 +0300

    Add caching module

--- lib/httpotion_cache.ex
+++
@@ -1,2 +0,0 @@
-defmodule HTTPotion.Cache do
-end
--- test/httpotion_cache_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HTTPotion.CacheTest do
-  use ExUnit.Case
-  doctest HTTPotion.Cache
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
